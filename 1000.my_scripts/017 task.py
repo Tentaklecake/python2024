ffcb448be52ea8925169ad64c7d010e603a51812
tentaklecake <tentaklecake@gmail.com>
2026-01-07 02:45:54 +0300

    [sol] 017 task added

---
+++ 1000.my_scripts/017 task.py
@@ -0,0 +1,20 @@
+# Task1: Плоский словарь
+# Создать два словаря c параметрами оборудования (ниже перечисление в виде "ключ = значение")
+
+device1 = dict(
+    hostname="r1.abcd.net",
+    ip="192.168.1.1",
+    username="cisco",
+    password="secret",
+    platform="cisco_ios",
+    enable=True,
+)
+
+device2 = dict(
+    hostname="sw1.abcd.net",
+    ip="192.168.1.2",
+    username="admin",
+    password="secret",
+    platform="huawei_vrp",
+    enable=False,
+)
